9f4eb047a916256768c8f2d8515b3be0ccbf7f68
allemannjonas@gmail.com <jonasallemann@gmx.ch>
2025-04-18 08:44:04 +0200

    plot cayley graph

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.graphstream:gs-core:2.0")
+    implementation("org.graphstream:gs-algo:2.0")
+    implementation("org.graphstream:gs-ui-swing:2.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,51 @@
 package org.example
 
 import java.util.*
+import org.graphstream.graph.implementations.SingleGraph
+import org.graphstream.ui.view.Viewer
 
 fun main() {
-//    val neutral = Permutation("(1)(2)(3)(4)(5)")
-//    val gen1 = Permutation("(1,2,3)(4,5)")
-//    val gen2 = Permutation("(1,2)(3,4,5)")
-    val neutral = Permutation("(1)(2)(3)(4)")
-    val gen1 = Permutation("(1)(2)(3,4)")
-    val gen2 = Permutation("(1,3,2,4)")
-    val gen3 = Permutation("(1,4,2,3)")
-    val generators = mutableListOf(gen1, gen2, gen3)
+    val generators = mutableListOf(
+//        Permutation("(1,2,3)(4,5)"),
+        Permutation("(1,2)(3,4,5)")
+    )
 
+    val cayleyGraph = generateCayleyGraph(generators)
+
+    println(cayleyGraph.size)
+
+    drawCayleyGraph(cayleyGraph)
+}
+
+private fun generateCayleyGraph(generators: MutableList<Permutation>): MutableMap<Permutation, List<Permutation>> {
     val queue: Queue<Permutation> = LinkedList()
     val graph = mutableMapOf<Permutation, List<Permutation>>()
-    queue.add(neutral)
+    queue.addAll(generators)
 
     while (queue.isNotEmpty()) {
         val current = queue.poll()
-        if(!graph.contains(current)) {
+        if (!graph.contains(current)) {
             val targets = generators.map { current * it }.distinct()
             graph[current] = targets
             queue.addAll(targets.filter { !graph.contains(it) })
         }
     }
+    return graph
+}
 
-    println(graph.size)
+private fun drawCayleyGraph(cayleyGraph: MutableMap<Permutation, List<Permutation>>) {
+    System.setProperty("org.graphstream.ui", "swing");
+    val graph = SingleGraph("Cayley Graph")
+    graph.isStrict = false
+    graph.setAutoCreate(true)
+
+    cayleyGraph.keys.forEach { graph.addNode(it.toString()) }
+    cayleyGraph.forEach { (k, v) ->
+        v.forEach { graph.addEdge("$k-$it", k.toString(), it.toString()) }
+    }
+
+    graph.setAttribute("ui.stylesheet", "node { fill-color: #aaffff; size: 20px; }")
+    val viewer: Viewer = graph.display()
+    viewer.enableAutoLayout()
+    viewer.enableXYZfeedback(true)
 }
