6643e8497ca1b5514bb64f8eb6642ade49b3029a
jonasallemann <jonas.allemann@oepfelbaum.com>
2025-06-26 14:43:29 +0200

    memory improvements

--- src/main/kotlin/SimpleThreeRubiksCube.kt
+++ src/main/kotlin/SimpleThreeRubiksCube.kt
@@ -6,14 +6,14 @@ import java.math.BigInteger
 class SimpleThreeRubiksCube {
 
     companion object {
-//        val F = Permutation("(1,3,8,6)(2,5,7,4)(14,25,35,24)(15,28,34,21)(16,30,33,19)")
-//        val L = Permutation("(17,19,24,22)(18,21,23,20)(9,1,33,41)(12,4,36,44)(14,6,38,46)")
+        val F = Permutation("(1,3,8,6)(2,5,7,4)(14,25,35,24)(15,28,34,21)(16,30,33,19)")
+        val L = Permutation("(17,19,24,22)(18,21,23,20)(9,1,33,41)(12,4,36,44)(14,6,38,46)")
         val R = Permutation("(25,27,32,30)(26,29,31,28)(16,48,40,8)(13,45,37,5)(11,43,35,3)")
         val U = Permutation("(9,11,16,14)(10,13,15,12)(46,27,3,19)(47,26,2,18)(48,25,1,17)")
     }
 
     private val dictionary = mapOf(
-//        F to "F",
+        F to "F",
 //        L to "L",
         R to "R",
         U to "U",
@@ -42,7 +42,7 @@ class SimpleThreeRubiksCube {
 
         var current = SchreierSims(generators, stabilizers)
         while(current.stabilizer != null) {
-            current = current.stabilizer
+            current = current.stabilizer!!
         }
         return current.stabilizerGenerators
     }
